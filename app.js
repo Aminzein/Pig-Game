a3a00b9369b4054ce1c1df4b6578737c3efff81a
Aminzein <Aminzein@users.Zforce.github.com>
2020-09-20 22:50:59 +0430

    Working on Game logic

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    diceDOM.style.display = 'none';
+}
+
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+function generateDiceNumber(){
+    return Math.floor(Math.random() * 6) + 1;
+} 
+
 var scores , roundScore , activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 1;
+init();
 var diceDOM =  document.querySelector('.dice');
 diceDOM.style.display = 'none';
 document.querySelector('.btn-roll').addEventListener('click' , function() {
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice = generateDiceNumber();
     diceDOM.src = 'dice-' + dice + '.png';
     diceDOM.style.display = 'block';
-    console.log(dice);
+    if(dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
+    else {
+        nextPlayer();
+    }
 });
+
+document.querySelector('.btn-hold').addEventListener('click' , () => {
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    if(scores[activePlayer] >= 100){
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        diceDOM.style.display = 'none';
+        document.querySelector('.player-' + activePlayer - '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer - '-panel').classList.remove('active');
+
+    }
+    else{
+        nextPlayer();
+    }
+})
+
+document.querySelector('.btn-new').addEventListener('click' , init); 
